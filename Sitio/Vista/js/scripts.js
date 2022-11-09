04c0789b694f4911b548d8da1ff5d891e1ec92dc
pululon <pululon@hotmail.com>
2022-11-09 18:49:20 -0300

    Trabajo de front-end

--- Sitio/Vista/js/scripts.js
+++ Sitio/Vista/js/scripts.js
@@ -33,3 +33,29 @@ $(window).on("load resize", function() {
     $dropdownMenu.addClass(showClass);
   }
 });
+
+function validateForm() {
+  let ctnombre = document.forms["ctform"]["ctnombre"].value;
+  let ctapellido = document.forms["ctform"]["ctapellido"].value;
+  let ctmail = document.forms["ctform"]["ctmail"].value;
+  let ctmensaje = document.forms["ctform"]["ctmensaje"].value;
+  let mensaje = "";
+  if (ctnombre == "") {
+    mensaje += "Debe completar el campo de nombre.";
+  }
+  if (ctapellido == "") {
+    mensaje += "\nDebe completar el campo de Apellido.";
+  }
+  if (ctmail == "") {
+    mensaje += "\nDebe completar el campo de mail para que podamos comunicarnos con usted.";
+  }
+  if (ctmensaje == "") {
+    mensaje += "\nDebe completar el campo de mensaje para tener conocimiento de sus dudas u opiniones.";
+  }
+  if (mensaje !== "") {
+    alert(mensaje);
+    return false;
+  }
+}
+
+
